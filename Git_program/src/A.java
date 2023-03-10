ec8afbd2548d70cde27cac3bb92635d27cd52ca2
Kartik <Kartik@LAPTOP-URIFTQSD>
2023-03-10 15:33:19 +0530

    By smita

---
+++ Git_program/src/A.java
@@ -0,0 +1,15 @@
+
+public class A
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
+		
+	}
+	public void m1()
+	{
+		
+		System.out.println("Hello-----Kartik");
+	}
+
+}
